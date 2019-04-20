428f6b94b1a820bafbb6dc71cf46387110f4113f
William Vabrinskas <wvabrinskas@gmail.com>
2019-04-20 11:29:01 -0400

    fixed channel selection

--- midi-controller/midi-controller.ino
+++ midi-controller/midi-controller.ino
@@ -5,50 +5,63 @@
 #define MIDI_CC_GENERAL1 10
 #define MIDI_CC MIDI_CC_GENERAL1
 #define MIDI_CHANNEL 3
+#define NUM_OF_INPUTS 5
 
 uint8_t oldAnalogValue = 0;
+
 uint8_t newAnalogValue = 0;
 
 typedef struct { 
-  uint8_t lang;
   int pin;
   uint16_t value;
-} valueDictionary;
+} valueStore;
 
-valueDictionary oldAnalogValues[] = {};
+valueStore oldAnalogValues[NUM_OF_INPUTS] = {};
 
-int pins[5] = {A0, A1, A2, A3, A6};
+int pins[NUM_OF_INPUTS] = {A0, A1, A2, A3, A6};
 
 void setup() {
-  for (int i = 0; i < sizeof(pins); i++) {
+  for (int i = 0; i < NUM_OF_INPUTS; i++) {
     int pin = pins[i];
     pinMode(pin, INPUT);
     uint16_t analogValue = analogRead(pin);
-    oldAnalogValues[i].pin = pin;
-    oldAnalogValues[i].value = analogValue;
+    valueStore oldValues = {pin , analogValue};
+    oldAnalogValues[i] = oldValues;
   }
   Serial.begin(9600);  
+}
+
+uint8_t getValue(int pin) {
+  for (int i = 0; i <= NUM_OF_INPUTS; i++) {
+    valueStore oldValues = oldAnalogValues[i];
+    if (oldValues.pin == pin) {
+      return oldValues.value;
+    }
+  }
+  return NULL;
 }
  
 void loop() {
 
   //probably should find a better way than a loop.
-  for (int i = 0; i < sizeof(pins); i++) { 
+  for (int i = 0; i < NUM_OF_INPUTS; i++) { 
+
     int pin = pins[i];
     uint16_t analogValue = analogRead(pin);
     uint8_t newAnalogValue = analogValue >> 3;
+    uint8_t oldAnalogValue = getValue(pin);
 
     if (newAnalogValue != oldAnalogValue) {   
-        midiEventPacket_t midiCc = {0x0B, 0xB0 | 0, i + 14, newAnalogValue};
+        midiEventPacket_t midiCc = {0x0B, 0xB0 | 0, i, newAnalogValue};
         MidiUSB.sendMIDI(midiCc);
         MidiUSB.flush();
-        oldAnalogValue = newAnalogValue;
         
+        oldAnalogValues[i] = { pin, newAnalogValue };
+
         Serial.print(i);
         Serial.print(": ");
         Serial.println(newAnalogValue);
 
-  
     }
   }
 }
