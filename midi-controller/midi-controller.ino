249a6e92f89c4e275344db0515b4545f4bf0beaa
William Vabrinskas <williamvabrinskas@Williams-MacBook-Pro.local>
2019-12-29 18:04:10 -0500

    added button inputs

--- midi-controller/midi-controller.ino
+++ midi-controller/midi-controller.ino
@@ -15,31 +15,64 @@ uint8_t newAnalogValue = 0;
 typedef struct { 
   int pin;
   uint16_t value;
-} valueStore;
+} SliderStore;
 
-valueStore oldAnalogValues[NUM_OF_SLIDERS] = {};
+typedef struct { 
+  int pin;
+  int value;
+} ButtonStore;
 
-int pins[NUM_OF_SLIDERS] = {A0, A1, A2, A3, A6};
+//array of type SliderStore with NUM_OF_SLIDERS count
+SliderStore oldAnalogValues[NUM_OF_SLIDERS] = {};
+ButtonStore oldDigitalValues[NUM_OF_BUTTONS] = {};
+
+int sliders[NUM_OF_SLIDERS] = {A0, A1, A2, A3, A6};
+int buttons[NUM_OF_BUTTONS] = {3, 4};
 
 void setup() {
   for (int i = 0; i < NUM_OF_SLIDERS; i++) {
-    int pin = pins[i];
+    int pin = sliders[i];
     pinMode(pin, INPUT);
     uint16_t analogValue = analogRead(pin);
-    valueStore oldValues = {pin , analogValue};
+    SliderStore oldValues = {pin , analogValue};
     oldAnalogValues[i] = oldValues;
   }
+
+  for (int i = 0; i < NUM_OF_BUTTONS; i++) {
+    int button = buttons[i];
+    pinMode(button, INPUT);
+      
+    int digitalValue = digitalRead(button);
+    ButtonStore oldValues = {button , digitalValue};
+    oldDigitalValues[i] = oldValues;
+  }
+
   Serial.begin(9600);  
 }
 
 uint8_t getValue(int pin) {
   for (int i = 0; i <= NUM_OF_SLIDERS; i++) {
-    valueStore oldValues = oldAnalogValues[i];
+    SliderStore oldValues = oldAnalogValues[i];
     if (oldValues.pin == pin) {
       return oldValues.value;
     }
   }
   return NULL;
+}
+
+int getButtonValue(int pin) {
+  for (int i = 0; i <= NUM_OF_BUTTONS; i++) {
+    ButtonStore oldValues = oldDigitalValues[i];
+    if (oldValues.pin == pin) {
+      return oldValues.value;
+    }
+  }
+  return NULL;
+}
+
+void sendMidi(midiEventPacket_t midiCc) {
+  MidiUSB.sendMIDI(midiCc);
+  MidiUSB.flush();
 }
  
 void loop() {
@@ -47,22 +80,39 @@ void loop() {
   //probably should find a better way than a loop.
   for (int i = 0; i < NUM_OF_SLIDERS; i++) { 
 
-    int pin = pins[i];
+    int pin = sliders[i];
     uint16_t analogValue = analogRead(pin);
     uint8_t newAnalogValue = analogValue >> 3;
     uint8_t oldAnalogValue = getValue(pin);
 
     if (newAnalogValue != oldAnalogValue) {   
         midiEventPacket_t midiCc = {0x0B, 0xB0 | 0, i + 0x1A, newAnalogValue};
-        MidiUSB.sendMIDI(midiCc);
-        MidiUSB.flush();
+        sendMidi(midiCc);
         
         oldAnalogValues[i] = { pin, newAnalogValue };
 
         Serial.print(i);
-        Serial.print(": ");
+        Serial.print(" slider: ");
         Serial.println(newAnalogValue);
 
     }
   }
+
+  for (int i = 0; i < NUM_OF_BUTTONS; i++) {
+    int button = buttons[i];
+    int newValue = digitalRead(button) >> 3;
+    int oldValue = getButtonValue(button);
+
+    if (newValue != oldValue) {
+        midiEventPacket_t midiCc = {0x44, 0xB0 | 0, i + 0x1A, newValue};
+        sendMidi(midiCc);
+
+        oldDigitalValues[i] = { button, newValue };
+
+        Serial.print(i);
+        Serial.print(" button: ");
+        Serial.println(newValue);
+    }
+  }
+
 }
